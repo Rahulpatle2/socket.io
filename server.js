1388451b1ca97571a8a21afb6e3c72c57ddb3263
rahulpatle2 <rahul25ec050@satiengg.in>
2025-03-12 14:43:08 +0530

    server.js file updated

--- server.js
+++ server.js
@@ -1,23 +1,33 @@
-const { Socket } = require('socket.io');
-const app = require('./src/app')
+const express = require('express');
+const app = express();
+const http = require('http');
+const path = require('path'); // Import the 'path' module
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
 
-const server = require('http').createServer(app);
-const io = require('socket.io')(server);
-io.on('connection', Socket => {
-  console.log('a user connected');
+// Serve static files from the 'public' directory (best practice)
+app.use(express.static(path.join(__dirname, 'public')));
 
-  Socket.on('disconnect',() =>{
-    console.log('a user disconnected');
-  })
-
-  Socket.on('some-event',(msg) =>{
-    console.log(msg);
-    console.log("some-event received");
-  })
+// Define a route for the root path ('/')
+app.get('/', (req, res) => {
+  // Construct the absolute path to index.html
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-server.listen(3000,() =>{
-    console.log('Server is running on port 3000')
-})
+io.on('connection', (socket) => {
+  console.log('a user connected');
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
+  socket.on('chat message', (msg) => {
+    io.emit('chat message', msg); // Broadcast to all connected clients
+    console.log('message: ' + msg);
+  });
+});
 
+const port = process.env.PORT || 3000; // Use environment variable or default
 
+server.listen(port, () => {  // Listen on the dynamically assigned PORT
+  console.log(`listening on *:${port}`);
+});
